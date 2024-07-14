fbb0a25b1d3204c7b10a2d0fdafec020765cdfcb
Dshan test <noobhacker3170@gmail.com>
2024-07-13 19:38:56 -0500

    File based routing
    I added a file based routing system for the about us section

---
+++ my-nextjs-app/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div className="p-24">
+      <h1 className="text-3xl font-bold mb-4">About Us</h1>
+      <p className="text-lg">
+        This is the about page of our Next.js 14 journey where I will learn nextjs.
+        Let's see it in development
+      </p>
+    </div>
+  )
+}
--- my-nextjs-app/app/page.tsx
+++ my-nextjs-app/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to Nextjs 14</h1>
       <p>This is my first Next js app using the app router</p>
+      <button className="mt-8 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">Learn More</button>
   </main>
   )
 }
